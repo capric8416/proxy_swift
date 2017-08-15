7fac15c01572d925212da33bb3a233ac4c7906b9
Capric <capric8416@gmail.com>
2017-08-15 18:22:52 +0800

    update protocol

--- proxy_swift/proxy_swift.py
+++ proxy_swift/proxy_swift.py
@@ -2,6 +2,7 @@
 # !/usr/bin/env python
 
 import hashlib
+import inspect
 import json
 import logging
 import time
@@ -24,14 +25,21 @@ class ProxySwift(object):
         self.url_change_ip = self.host + '/ip/change'
 
     def get_ip(self, interface_id=None, pool_id=1):
-        return self._requests_get(
-            url=self.url_get_ip,
-            data={
-                'server_id': self.server_id,
-                'pool_id': pool_id,
-                'interface_id': interface_id or '',
-            }
-        )
+        i = 1
+        while True:
+            result = self._requests_get(
+                url=self.url_get_ip,
+                data={
+                    'server_id': self.server_id,
+                    'pool_id': pool_id,
+                    'interface_id': interface_id or '',
+                }
+            )
+            if result['code'] == 200:
+                return result['data']
+            else:
+                self._log('error', inspect.currentframe().f_code.co_name, result)
+                time.sleep(i % 2 + 1)
 
     def change_ip(self, interface_id, pool_id=1, _filter=24):
         task_id = self._change_ip(interface_id=interface_id, _filter=_filter)
@@ -40,27 +48,51 @@ class ProxySwift(object):
         while True:
             time.sleep(i % 2 + 1)
 
-            data = self._get_task(task_id)
-            if data['status'] == 'failed':
-                logger.error(data)
+            if not self._get_task(task_id=task_id):
                 task_id = self._change_ip(interface_id=interface_id, _filter=_filter)
                 continue
-            elif data['status'] != 'success':
-                logger.warning(data)
-                continue
 
-            ip_port = self.get_ip(pool_id=pool_id, interface_id=interface_id)
-            return ip_port[0]
+            return self.get_ip(pool_id=pool_id, interface_id=interface_id)[0]
 
     def _change_ip(self, interface_id, _filter):
-        return self._requests_get(
-            url=self.url_change_ip,
-            data={
-                'server_id': self.server_id,
-                'interface_id': interface_id,
-                'filter': _filter,
-            }
-        )['taskId']
+        i = 1
+        while True:
+            result = self._requests_get(
+                url=self.url_change_ip,
+                data={
+                    'server_id': self.server_id,
+                    'interface_id': interface_id,
+                    'filter': _filter,
+                }
+            )
+
+            task_id = result['data'].get('task_id')
+            if result['code'] == 202 and task_id:
+                return task_id
+            else:
+                self._log('error', inspect.currentframe().f_code.co_name, result)
+                time.sleep(i % 2 + 1)
+
+    def _get_task(self, task_id):
+        i = 1
+        while True:
+            result = self._requests_get(
+                url=self.url_get_task,
+                data={'task_id': task_id}
+            )
+
+            status = result['data']['status']
+            if result['code'] == 200:
+                if status == 'success':
+                    return True
+                elif status == 'failed':
+                    self._log('error', inspect.currentframe().f_code.co_name, result)
+                    return False
+                else:
+                    self._log('warning', inspect.currentframe().f_code.co_name, result)
+            else:
+                self._log('error', inspect.currentframe().f_code.co_name, result)
+                time.sleep(i % 2 + 1)
 
     def _requests_get(self, url, data):
         source_data = {
@@ -85,11 +117,8 @@ class ProxySwift(object):
 
         return requests.get(url, params=source_data, verify=False, json=True).json()
 
-    def _get_task(self, task_id):
-        return self._requests_get(
-            url=self.url_get_task,
-            data={'task_id': task_id}
-        )
+    def _log(self, level, method_name, message):
+        return getattr(logger, level)(f'{self.__class__.__name__}.{method_name}: {message}')
 
 
 if __name__ == '__main__':
@@ -98,3 +127,5 @@ if __name__ == '__main__':
     _data = _proxy_swift.get_ip(pool_id=1)  # 获取1号池所有ip
     print(json.dumps(obj=_data, indent='\t', ensure_ascii=False))
 
+    _data = _proxy_swift.change_ip(interface_id=23)  # 更换23号ip
+    print(json.dumps(obj=_data, indent='\t', ensure_ascii=False))
